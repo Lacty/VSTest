c5e464db1b67cd8a0de6a1c713f3a4e4e360ce40
Lacty <akira206@gmail.com>
2015-04-21 00:28:00 +0900

    vactor test

--- TestRoom/main.cpp
+++ TestRoom/main.cpp
@@ -1,18 +1,14 @@
 
 #include <iostream>
+#include <vector>
 
 
 int main(){
+  std::vector<int> va;
 
-  int a[10] = {0,1,2,3,4,5,6,7,8,9};
-
-  for (auto& b : a){
-    b += 1;
-  }
-
-
-  for (auto b : a){
-    std::cout << b << std::endl;
+  for (int i = 0; i < 10; ++i) {
+    va.emplace_back(i);
+    std::cout << va.at(i) << std::endl;
   }
 
   return 0;
