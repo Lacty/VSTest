b09c13661a2532100e493da1def360c02cfa6bff
Lacty <akira206@gmail.com>
2015-04-21 14:08:15 +0900

    新井さんうひょああああああ

--- TestRoom/main.cpp
+++ TestRoom/main.cpp
@@ -5,18 +5,8 @@
 
 
 int main(){
-  std::map<int, std::string> map_test;
 
-  int ID_test = 0;
-
-  std::string str_temp;
-  str_temp = "Visual Studio is God";
-
-  map_test.insert(std::make_pair(ID_test, str_temp));
-
-  auto it_temp = map_test.find(ID_test);
-
-  std::cout << it_temp->second << std::endl;
+  // Vˆä‚³‚ñ‚¤‚Ğ‚å‚ ‚ ‚ ‚ ‚ ‚ ‚ 
 
   return 0;
 }
